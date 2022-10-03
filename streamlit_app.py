1cbce011896fcb14e1b8807cd111a56e637915cd
igopis <114951474+igopis@users.noreply.github.com>
2022-10-03 15:52:10 -0400

    Update streamlit_app.py
    added second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard Boiled Egg')
 streamlit.text('Avocado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
