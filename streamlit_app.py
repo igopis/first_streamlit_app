797608f1b674eeef01f0ad3f4aed0887b2c9789e
igopis <114951474+igopis@users.noreply.github.com>
2022-10-03 15:58:19 -0400

    Update streamlit_app.py
    added fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('Hard Boiled Egg')
 streamlit.text('Avocado Toast')
 
 streamlit.header('Build Your Own Fruit Smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
